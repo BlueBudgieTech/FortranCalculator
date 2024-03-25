069934429799a4fbb3a09912c3870c5364b455e9
ThatBlueBudgie <85682026+BlueBudgieTech@users.noreply.github.com>
2024-03-25 12:56:23 +0400

    Update Calc.f90
    Updated readability and edited for better code formatting

--- Calc.f90
+++ Calc.f90
@@ -4,25 +4,28 @@ program Calculator
 	real :: num2
 	character(len=3) :: keyword
 
-		print *, 'Enter first number: '
+		write (*, "(A)", advance = "no") "Enter first number: "
 		read(*,*) num1
 
-		print *, 'Enter second number: '
+		write (*, "(A)", advance = "no") "Enter second number: "
 		read(*,*) num2
 		
-		print *, 'What would you like to do? (Add, Subtract[Sub], Multiply[Mul] or Divide[Div])?'
+		write (*, "(A)", advance = "no") "What would you like to do? (Add, Subtract[Sub], Multiply[Mul] or Divide[Div])?: "
 		read(*,*) keyword
 
+		! F0.3 reduces the number of decimal places to be 3
+  		! For other edit descriptors, check out https://fortranwiki.org/fortran/show/Edit+descriptors
+    
 		if (keyword == "Add") then
-			print *,'Sum of ', num1, ' and ', num2, ' is = ', (num1+num2)
+   			print '("Sum = ", f0.3)', (num1 + num2)
 		else if (keyword == "Sub") then
-			print *,'Difference of ', num1, ' and ', num2, ' is = ', (num1-num2)
+			print '("Difference = ", f0.3)', (num1 - num2)
 		else if (keyword == "Mul") then
-			print *,'Product of ', num1, ' and ', num2, ' is = ', (num1*num2)
+			print '("Product = ", f0.3)', (num1 * num2)
 		else if (keyword == "Div") then
-			print *,'Quotient of ', num1, ' and ', num2, ' is = ', (num1/num2)
+			print '("Quotient = ", f0.3)', (num1 / num2)
 		else
-			print *, 'Invalid Input'
+			write (*, "(A)", advance = "no") "Invalid Input"
 		end if
 
 		read(*,*)
